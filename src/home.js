dc55368691f586d7f35385cc942456f397d74863
gokel166 <okellogeorge40@yahoo.com>
2018-02-07 02:47:30 -0600

    add react components for basic home page UI

---
+++ src/home.js
@@ -0,0 +1,5 @@
+export default () => {
+    <div>
+        <h1>Hello To the Home</h1>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hello and welcome to electron');
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+    <div>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+        </ul>
+
+        <hr/>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+  document.getElementById('app')
+);
