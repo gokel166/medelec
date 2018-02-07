2794394ce4a2289d3677ba83512343c9c115ca11
gokel166 <okellogeorge40@yahoo.com>
2018-02-07 01:57:54 -0600

    add and modify serve js

--- serve.js
+++ serve.js
@@ -12,7 +12,47 @@ const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 
 config.plugins = [
-    new webpack.DefinePlugin({
-        'process.env': {NODE_ENV: JSON.stringify(process.env.NODE_ENV)},
-    }),
+  new webpack.DefinePlugin({
+    'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
+  }),
 ];
+
+//setup hot reload
+config.plugins.push(new webpack.HotModuleReplacementPlugin());
+//setup no errors-plugin
+config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
+//override entry for hot reload
+config.entry = ['webpack-hot-middleware/client', config.entry];
+
+//return compiler instance
+const compiler = webpack(config);
+//stats output config
+const statsConfig = {
+  colors: true,
+  hash: false,
+  timings: true,
+  chunks: false,
+  chunksModules: false,
+  modules: false,
+};
+
+app.use(
+  webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: statsConfig,
+  })
+);
+app.use(webpackHotMiddleware(compiler));
+
+//serve static files
+app.use(express.static(__dirname));
+//serve index
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+//start server
+app.listen(3000, err => {
+  if (err) {
+    console.log(err);
+  }
+  console.info('===> Listening to port 3000');
+});
