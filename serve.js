629e13fc61c0b594bdc77406b95d713f57f0e443
gokel166 <okellogeorge40@yahoo.com>
2018-02-07 02:31:36 -0600

    complete webpack and react

--- serve.js
+++ serve.js
@@ -7,52 +7,55 @@ const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config');
 
-const app = express();
+module.exports = () => {
+  //create express
+  const app = express();
 
-const isProduction = process.env.NODE_ENV === 'production';
+  const isProduction = process.env.NODE_ENV === 'production';
 
-config.plugins = [
-  new webpack.DefinePlugin({
-    'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
-  }),
-];
+  config.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
+    }),
+  ];
 
-//setup hot reload
-config.plugins.push(new webpack.HotModuleReplacementPlugin());
-//setup no errors-plugin
-config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
-//override entry for hot reload
-config.entry = ['webpack-hot-middleware/client', config.entry];
+  //setup hot reload
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  //setup no errors-plugin
+  config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
+  //override entry for hot reload
+  config.entry = ['webpack-hot-middleware/client', config.entry];
 
-//return compiler instance
-const compiler = webpack(config);
-//stats output config
-const statsConfig = {
-  colors: true,
-  hash: false,
-  timings: true,
-  chunks: false,
-  chunksModules: false,
-  modules: false,
+  //return compiler instance
+  const compiler = webpack(config);
+  //stats output config
+  const statsConfig = {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunksModules: false,
+    modules: false,
+  };
+
+  app.use(
+    webpackMiddleware(compiler, {
+      publicPath: config.output.publicPath,
+      contentBase: 'src',
+      stats: statsConfig,
+    })
+  );
+  app.use(webpackHotMiddleware(compiler));
+
+  //serve static files
+  app.use(express.static(__dirname));
+  //serve index
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+  //start server
+  app.listen(3000, err => {
+    if (err) {
+      console.log(err);
+    }
+    console.info('===> Listening to port 3000');
+  });
 };
-
-app.use(
-  webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    contentBase: 'src',
-    stats: statsConfig,
-  })
-);
-app.use(webpackHotMiddleware(compiler));
-
-//serve static files
-app.use(express.static(__dirname));
-//serve index
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-//start server
-app.listen(3000, err => {
-  if (err) {
-    console.log(err);
-  }
-  console.info('===> Listening to port 3000');
-});
