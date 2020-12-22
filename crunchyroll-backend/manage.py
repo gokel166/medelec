5dbe92552f7b9bfc5c5a0f128fa93fa4afd15aed
gokel166 <okellogeorge40@yahoo.com>
2020-12-21 21:18:53 -0600

    Add docker container for postgres services for user admin

--- crunchyroll-backend/manage.py
+++ crunchyroll-backend/manage.py
@@ -5,10 +5,21 @@ from app import app
 manager = Manager(app)
 
 
+class HiThere(Command):
+    def run(self, name):
+        print('Hi There %d' % name)
+
+
 @manager.command
 def hello():
     print('Hello')
 
+
+def dropdb():
+    print('Dropped Db')
+
+
+manager.add_command({'Hi There': HiThere()})
 
 if __name__ == "__main__":
     manager.run()
